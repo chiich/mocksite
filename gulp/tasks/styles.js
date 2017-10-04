5f8eb1cb20d2909fe756aa851ef996a2b266bc41
Chie <ichongiribc@gmail.com>
2017-10-04 09:46:48 +0100

    Completed styling the header for Desktop sizes

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,11 +6,12 @@ var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
 var mixins = require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/style.css')
-              .pipe(postcss([cssimport, mixins, nested, cssvars, autoprefixer]))
+              .pipe(postcss([cssimport, mixins, nested, cssvars,hexrgba, autoprefixer]))
               .on('error', function(errorInfo){
                 console.log(errorInfo.toString());
                 this.emit('end');
