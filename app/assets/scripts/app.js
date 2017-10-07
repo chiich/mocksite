028fabbb192041c043398819987c8abac20c3515
Chie <ichongiribc@gmail.com>
2017-10-07 12:35:45 +0100

    finished base styling for menu icon - which is still a square

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,17 +1,3 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    payTexes() {
-        console.log(this.name + ' now owes £0 in taxes')
-    }
-}
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTexes();
-
-$(".headline").first().remove();
+var mobileMenu = new MobileMenu();
